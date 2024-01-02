0dcf0ec09c7c4eeaae5bf90c79b2276d8d20b487
ishola360 <isholah360.com>
2024-01-02 01:30:56 -0800

    third commit

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const Alexa = require("ask-sdk-core");
 const router = require("./route/route");
 
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.set("view engine", "ejs")
 app.use(cors());
 app.use("/nizams", require("./nizams"));
 app.use("/keventers", require("./keventers"));
--- controller/controlle.js
+++ controller/controlle.js
@@ -1,15 +1,22 @@
+const express = require("express");
+const app = express();
 
+
+app.use(express.urlencoded({extended:true}))
+app.set("view engine", "ejs")
 
 
 const adminPanel =  (req, res) => {
 
     try {
+       
+        res.render('admin')
         res.status(201).json({"message": "this is the admin panel"})
      
     } catch (error) {
       res.status(400).json("Invalid User Credential");
-    }
-    
+   }
+  
   };
-
+ 
   module.exports = { adminPanel,  };
